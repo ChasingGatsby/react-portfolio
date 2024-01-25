04dd36ba69f9f7a4d0299f4e21272c8e8fe9507d
Gatsbeau <jabriel0ne@gmail.com>
2024-01-24 23:10:43 -0500

    Added downloadable resume link

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -5,7 +5,9 @@ function NavTabs() {
 
   return (
     <ul className="nav nav-tabs d-flex align-items-end">
-      <li className="navbar-brand title p-3 mx-3 align-self-center">Gatsbeau SUCKS at Coding!</li>
+      <li className="navbar-brand title p-3 mx-3 align-self-center">
+        Gatsbeau SUCKS at Coding!
+      </li>
       <li className="nav-item">
         <Link
           to="/About"
@@ -21,7 +23,9 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/Projects"
-          className={currentPage === "/Projects" ? "nav-link active" : "nav-link"}
+          className={
+            currentPage === "/Projects" ? "nav-link active" : "nav-link"
+          }
         >
           Projects
         </Link>
@@ -36,7 +40,16 @@ function NavTabs() {
           Contact
         </Link>
       </li>
-      
+      <li className="nav-item d-flex">
+        <a
+          href="src/assets/resume.pdf"
+          download
+          className="nav-link active align-self-center"
+          style={{ backgroundColor: "#7D0A0A", color: "white" }}
+        >
+          Download Resume
+        </a>
+      </li>
     </ul>
   );
 }
